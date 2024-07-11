1ab7961fa61c6ea9626302feefed0ca3b4f07c33
laksmi santo <84493741+laksmisanto@users.noreply.github.com>
2024-07-11 23:27:25 +0600

    update-content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="font-sans text-sm text-gray-100 h-8 bg-sky-600 flex justify-center items-center">
-      <p>create by laksmi santo</p>
+      <p>
+        &copy;copyright-2024 created by laksmi santo ||
+        laksmisanto1998@gmail.com || +8801793940504 || Thakurgaon Sadar,
+        Thakurgaon
+      </p>
     </div>
   );
 };
